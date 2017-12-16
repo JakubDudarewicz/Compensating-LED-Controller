9360c805528028640d061e681b32dcd9a9d77832
Jakub Dudarewicz <jakub.dudarewicz@gmail.com>
2017-12-16 14:00:54 +0100

    Add working i2c and RTC libraries

---
+++ CompensatingController/RTC.c
@@ -0,0 +1,112 @@
+/*
+ * RTC.c
+ *
+ * Created: 14.12.2017 13:36:35
+ *  Author: Jakub Dudarewicz
+ */ 
+
+#include "i2c.h"
+#include "RTC.h"
+#include <avr/io.h>
+
+void sendToAdress(uint8_t address, uint8_t data)
+{
+	uint8_t buf[2];
+	buf[0] = address;
+	buf[1] = data;
+	i2cMasterSendNI(RTC_ADDRESS, 2, &buf);
+}
+
+uint8_t* getFromAdress(uint8_t address, uint8_t consecutive)
+{
+	uint8_t *buf = malloc(consecutive);
+	buf[0] = address;
+	i2cMasterSendNI(RTC_ADDRESS, 1, buf);
+	i2cMasterReceiveNI(RTC_ADDRESS, consecutive, buf);
+	return buf;
+}
+
+void RTCStart()
+{
+	uint8_t* buf = getFromAdress(RTCSEC, 1);
+	sendToAdress(RTCSEC, _BV(RTCSEC_ST) | (*buf & RTCSEC_MASK));
+}
+
+void RTCStop()
+{
+	uint8_t* buf = getFromAdress(RTCSEC, 1);
+	sendToAdress(RTCSEC, *buf & RTCSEC_MASK);
+}
+
+void setTime(uint8_t hours, uint8_t minutes, uint8_t seconds)
+{
+	uint8_t* buf = getFromAdress(RTCSEC, 1);
+	sendToAdress(RTCSEC, BIN2BCD(seconds) | (*buf & _BV(RTCSEC_ST)));
+	sendToAdress(RTCMIN, BIN2BCD(minutes));
+	sendToAdress(RTCHOUR, BIN2BCD(hours));
+	free(buf);
+}
+
+void setDate(uint8_t year, uint8_t month, uint8_t day)
+{
+	sendToAdress(RTCDATE, BIN2BCD(day) & RTCDATE_MASK);
+	sendToAdress(RTCMTH, BIN2BCD(month) & RTCMTH_MASK);
+	sendToAdress(RTCYEAR, BIN2BCD(year));
+}
+
+uint8_t getTime(UNIT u)
+{
+	uint8_t address = RTCSEC;
+	switch (u)
+	{
+		case SECONDS:
+		address = RTCSEC;
+		break;
+		case MINUTES:
+		address = RTCMIN;
+		break;
+		case HOURS:
+		address = RTCHOUR;
+		break;
+		case WEEKDAY:
+		address = RTCWKDAY;
+		break;
+		case MONTH:
+		address = RTCMTH;
+		break;
+		case YEAR:
+		address = RTCYEAR;
+		break;
+		case LEAP:
+		address = RTCYEAR;
+		break;
+	}
+	uint8_t *data = getFromAdress(address, 1);
+	switch (u)
+	{
+		case SECONDS:
+		return BCD2BIN(*data & RTCSEC_MASK);
+		case MINUTES:
+		return BCD2BIN(*data & RTCMIN_MASK);
+		case HOURS:
+		return BCD2BIN(*data & RTCHOUR_MASK);
+		case WEEKDAY:
+		return BCD2BIN(*data & RTCWKDAY_MASK);
+		case MONTH:
+		return BCD2BIN(*data & RTCMTH_MASK);
+		case YEAR:
+		return BCD2BIN(*data);
+		case LEAP:
+		return *data & _BV(RTCMTH_LPYR);
+	}
+}
+
+void RTCConfigure(uint8_t settingByte)
+{
+	sendToAdress(CONTROL, settingByte);
+}
+
+void set24HFormat()
+{
+	
+}
---
+++ CompensatingController/RTC.h
@@ -0,0 +1,96 @@
+/*
+ * RTC.h
+ *
+ * Created: 14.12.2017 13:36:56
+ *  Author: Jakub Dudarewicz
+ */ 
+
+
+#ifndef FILE
+#define FILE
+
+#define TRUE			1
+#define FALSE			0
+
+#define LOCAL_ADDR		0xA0
+#define RTC_ADDRESS		0xDE
+#define BUFFER_SIZE		20
+
+#define RTCSEC			0x00
+#define RTCMIN			0x01
+#define	RTCHOUR			0x02
+#define RTCWKDAY		0x03
+#define	RTCDATE			0x04
+#define	RTCMTH			0x05
+#define	RTCYEAR			0x06
+#define	CONTROL			0x07
+#define OSCTRIM			0x08
+#define ALM0SEC			0x0A
+#define	ALM0MIN			0x0B
+#define	ALM0HOUR		0x0C
+#define	ALM0WKDAY		0x0D
+#define ALM0DATE		0x0E
+#define	ALM0MTH			0x0F
+#define ALM1SEC			0x11
+#define	ALM1MIN			0x12
+#define	ALM1HOUR		0x13
+#define	ALM1WKDAY		0x14
+#define ALM1DATE		0x15
+#define	ALM1MTH			0x16
+
+#define	RTCSEC_ST			7
+#define RTCHOUR_FORMAT		6
+#define RTCHOUR_AMPM		5
+#define RTCMTH_LPYR			5
+#define RTCYEAR_TRIMSIGN	7
+#define	RTCWKDAY_OSCRUN		5
+#define CONTROL_OUT			7
+#define CONTROL_SQWEN		6
+#define CONTROL_ALM1EN		5
+#define CONTROL_ALM0EN		4
+#define CONTROL_EXTOSC		3
+#define CONTROL_CRSTRIM		2
+#define CONTROL_SQWFS1		1
+#define CONTROL_SQWFS0		0
+#define	OSCTRIM_SIGN		7
+
+#define	RTCSEC_MASK		0x7F
+#define	RTCMIN_MASK		0x7F
+#define	RTCHOUR_MASK	0x3F
+#define RTCWKDAY_MASK	0x07
+#define	RTCDATE_MASK	0x3F
+#define	RTCMTH_MASK		0x1F
+#define	TRIMVAL_MASK	0x7F
+
+#define BCD2BIN(bcd)	(bcd & 0x0F) + (((bcd & 0xF0) / 16) * 10)
+#define	BIN2BCD(bin)	((bin / 10) * 16) | (bin % 10)
+
+typedef enum {
+	SECONDS,
+	MINUTES,
+	HOURS,
+	WEEKDAY,
+	MONTH,
+	YEAR,
+	LEAP
+}UNIT;
+
+void sendToAdress(uint8_t address, uint8_t data);
+uint8_t* getFromAdress(uint8_t address, uint8_t consecutive);
+void RTCStart();
+void RTCStop();
+void set12HFormat();
+void set24HFormat();
+void isOSCRunning();
+void isLeap();
+//SQWFS setting
+//00 - 1Hz
+//01 - 4.096kHz
+//10 - 8.192kHz
+//11 - 32.768kHz
+void RTCConfigure(uint8_t settingByte);
+void setTime(uint8_t hours, uint8_t minutes, uint8_t seconds);
+void setDate(uint8_t year, uint8_t month, uint8_t day);
+uint8_t getTime(UNIT u);
+
+#endif /* INCFILE1_H_ */
