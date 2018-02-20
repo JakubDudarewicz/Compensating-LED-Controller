b495be03ff35094751f39e57f138f8897b4c717e
ajzo-15 <ajzo-15@sr.dmcs.pl>
2018-02-20 19:45:13 +0100

    Add sensor descriptions and comments

--- CompensatingController/headers/sensor.h
+++ CompensatingController/headers/sensor.h
@@ -1,9 +1,15 @@
-/*
- * sensor.h
- *
- * Created: 2018-01-11 7:11:30 PM
- *  Author: Jakub Dudarewicz
- */ 
+/*******************************************************
+Title:			TCS3200D light sensor library
+				signal frequency interface
+Filename:		'sensor.h'
+Author:			Jakub Dudarewicz
+Version:		0.1
+Created:		12-2017
+Target MCU:		Atmel AVR
+
+This code is distributed under the GNU Public License
+which can be found at http://www.gnu.org/licenses/gpl.txt
+*******************************************************/
 
 #ifndef SENSOR_H_
 #define SENSOR_H_
@@ -22,6 +28,7 @@ typedef enum{
 	CLEAR,
 	GREEN
 }SENSOR_COLOR;
+//filter color identification
 
 int sensorScan(SENSOR_COLOR color, int accuracy);
 
--- CompensatingController/source files/sensor.c
+++ CompensatingController/source files/sensor.c
@@ -1,9 +1,15 @@
-/*
- * sensor.c
- *
- * Created: 2018-01-11 7:11:01 PM
- *  Author: Jakub Dudarewicz
- */ 
+/*******************************************************
+Title:			TCS3200D light sensor library
+				signal frequency interface
+Filename:		'sensor.c'
+Author:			Jakub Dudarewicz
+Version:		0.1
+Created:		12-2017
+Target MCU:		Atmel AVR
+
+This code is distributed under the GNU Public License
+which can be found at http://www.gnu.org/licenses/gpl.txt
+*******************************************************/
  
 #include <avr/io.h>
 #include "sensor.h"
@@ -11,8 +17,7 @@
  int sensorScan(SENSOR_COLOR color, int accuracy)
  {
 	 int buf = 0, i = 0;
-	 TIFR0 |= _BV(TOV0);
-	 switch (color)
+	 switch (color) //color filter selection
 	 {
 		 case CLEAR:
 		 SENSOR_PORT = _BV(SENSOR_S2);
@@ -29,11 +34,11 @@
 	 }
 	 while(buf <= accuracy)
 	 {
-		 while (SENSOR_PIN & _BV(SENSOR_OUT));
-		 while (!(SENSOR_PIN & _BV(SENSOR_OUT))) buf++;
-		 i++;
+		 while (SENSOR_PIN & _BV(SENSOR_OUT)); //wai for signal low
+		 while (!(SENSOR_PIN & _BV(SENSOR_OUT))) buf++; //count while signal low
+		 i++; //increment number of period counts
 	 }
 	 SENSOR_PORT = 0;
-	 if (i < 2) return SENSOR_OVF;
-	 else return accuracy - (buf / i);
+	 if (i < 2) return SENSOR_OVF; //if too dark and measurement unreliable, return error code
+	 else return accuracy - (buf / i); //average counts by periods
  }
